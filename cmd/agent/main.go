fdb7539587699249d0c234c65a813eb24bc1e2b1
apneist-dev <odxe@yandex.ru>
2025-03-09 22:35:45 +0300

    config added

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,53 +1,17 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
+	agentconfig "metralert/config/agent"
 	"metralert/internal/agent"
-
-	"github.com/caarlos0/env/v6"
 )
 
 func main() {
-	type Config struct {
-		ServerAddress  string `env:"ADDRESS"`
-		ReportInterval int    `env:"REPORT_INTERVAL"`
-		PollInterval   int    `env:"pollInterval"`
-	}
-	var (
-		cfg            Config
-		serverurl      *string
-		reportInterval *int
-		pollInterval   *int
-	)
+	cfg := agentconfig.Config{}
+	cfg.GetConfig()
 
-	err := env.Parse(&cfg)
-	if err != nil {
-		fmt.Println("Переменная окружения ADDRESS не определена")
-	}
-	if cfg.ServerAddress == "" {
-		serverurl = flag.String("a", "localhost:8080", "server url")
-	} else {
-		serverurl = &cfg.ServerAddress
-	}
-
-	if cfg.ReportInterval == 0 {
-		reportInterval = flag.Int("r", 10, "reportInterval")
-	} else {
-		reportInterval = &cfg.ReportInterval
-	}
-
-	if cfg.PollInterval == 0 {
-		pollInterval = flag.Int("p", 10, "pollInterval")
-	} else {
-		pollInterval = &cfg.PollInterval
-	}
-
-	flag.Parse()
-
-	log.Printf("Запущен сервер с адресом %s", *serverurl)
-	client := agent.NewClient(*serverurl, *pollInterval, *reportInterval)
+	log.Printf("Запущен агент:\nServerAddress %s,\nPollInterval: %d,\nReportInterval: %d", cfg.ServerAddress, cfg.PollInterval, cfg.ReportInterval)
+	client := agent.NewClient(cfg.ServerAddress, cfg.PollInterval, cfg.ReportInterval)
 	go agent.CollectMetric()
 	go client.SendAllMetrics()
 
