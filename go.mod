07d826abb5faf30da49fc141f914eb3497413571
apneist-dev <odxe@yandex.ru>
2025-11-05 01:08:24 +0300

    iter21. generate init

--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/exp v0.0.0-20251023183803-a4bb9ffd2546 // indirect
 	golang.org/x/mod v0.29.0 // indirect
 	golang.org/x/net v0.46.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -3,6 +3,7 @@ package metrics
 type Gauge float64
 type Counter int64
 
+// generate:reset
 type Metrics struct {
 	ID    string   `json:"id"`              // имя метрики
 	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
@@ -10,8 +11,19 @@ type Metrics struct {
 	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 }
 
+// generate:reset
 type AuditMetrics struct {
 	TS          int64    `json:"ts"`
 	MetricNames []string `json:"metrics"`
 	IP          string   `json:"ip_address"`
+}
+
+// generate:reset
+type ResetableStruct struct {
+	i     int
+	str   string
+	strP  *string
+	s     []int
+	m     map[string]string
+	child *ResetableStruct
 }
